5cd0b2bda2bfbee467d3471a4bdb109158b28190
Ibrar Alam <ibby1561@gmail.com>
2022-12-21 20:26:45 +0530

    login-signup files updated

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Nav from './components/Nav';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-    <Nav/>
+      <Nav />
       <Routes>
-      <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<SignUp/>} />
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
